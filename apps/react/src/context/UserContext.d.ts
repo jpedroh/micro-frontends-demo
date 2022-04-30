f889bb7cd0a6fe37346f4b0b809bb9bff87687f4
jpedroh <joao.pedro.hsd@gmail.com>
2022-04-30 03:04:53 +0000

    Deploying to gh-pages from  @ 71352766de0d170e4f4edea4b1383779c553e9b3 🚀

--- apps/react/src/context/UserContext.d.ts
+++ apps/react/src/context/UserContext.d.ts
@@ -1,12 +1,7 @@
 import { User } from "@micro-frontends-demo/auth";
-import React, { FC } from "react";
+import { FC } from "react";
 import { Observable } from "rxjs";
-declare type UserContextData = {
-    user: User | undefined;
-};
-export declare const UserContext: React.Context<UserContextData>;
 export declare const UserProvider: FC<{
     userStream: Observable<User>;
 }>;
 export declare const useUser: () => User;
-export {};
